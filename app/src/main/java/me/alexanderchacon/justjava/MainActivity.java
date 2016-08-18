6574e5591f50c54501e16b8d3be96aeccd34ab34
Alexander Chacon <helloalexanderchacon@gmail.com>
2016-08-17 20:37:05 -0500

    Fixed and functional horizontal picker

--- app/src/main/java/me/alexanderchacon/justjava/MainActivity.java
+++ app/src/main/java/me/alexanderchacon/justjava/MainActivity.java
@@ -1,8 +1,22 @@
-package me.alexanderchacon.justjava;
+package me.alexanderchacon.justjava; /**
+ * Add your package below. Package name can be found in the project's AndroidManifest.xml file.
+ * This is the package name our example uses:
+ *
+ * package com.example.android.justjava; 
+ */
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
 
+import java.text.NumberFormat;
+
+import me.alexanderchacon.justjava.R;
+
+/**
+ * This app displays an order form to order coffee.
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -10,4 +24,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+
+    int quantity = 0;
+
+    public void submitOrder(View view) {
+        displayPrice(quantity * 5);
+        display(quantity);
+    }
+
+    public void increment(View View){
+        quantity = quantity + 1;
+        display(quantity);
+
+    }
+
+    public void decrement(View View){
+        quantity = quantity -1;
+        display(quantity);
+
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
 }
